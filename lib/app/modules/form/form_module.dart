253fb674a7c5f3cc0ed52a6519ec1ad3b374d607
Pedro Lemos <lemozxpedro@gmail.com>
2021-02-09 13:16:58 -0300

    feat: optimized imports

--- lib/app/modules/form/form_module.dart
+++ lib/app/modules/form/form_module.dart
@@ -1,11 +1,11 @@
-import 'package:first_challenge/app/modules/form/domain/usecases/upload_file_to_web.dart';
-import 'package:first_challenge/app/modules/form/external/datasources/upload_datasource_implementation.dart';
-import 'package:first_challenge/app/modules/form/infrastructure/repositories/upload_repository_implementation.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 import 'domain/usecases/get_pdf_from_storage.dart';
+import 'domain/usecases/upload_file_to_web.dart';
 import 'external/datasources/pdf_datasource_implementation.dart';
+import 'external/datasources/upload_datasource_implementation.dart';
 import 'infrastructure/repositories/pdf_repository_implementation.dart';
+import 'infrastructure/repositories/upload_repository_implementation.dart';
 import 'presentation/controllers/form_bloc.dart';
 import 'presentation/pages/form_page.dart';
 import 'presentation/pages/success_page.dart';
